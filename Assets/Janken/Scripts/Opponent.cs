d336529f3eaa130f46da96bb555f879f8ca2f836
KengoKashihara <KengoKashihara1998@gmail.com>
2020-12-11 00:59:46 +0900

    対戦相手の手のローテーション速度を調整できるように実装
    対戦相手の手が変化する間隔を秒数で指定できるように修正

--- Assets/Janken/Scripts/Opponent.cs
+++ Assets/Janken/Scripts/Opponent.cs
@@ -8,7 +8,9 @@ public class Opponent : MonoBehaviour
     private readonly string[] HandTexts = { "グー", "チョキ", "パー" };
 
     [SerializeField] private Text handText = null;
+    [SerializeField] private float changeTime = 0.0f;
     private int handNum = 0;
+    private float changeCount = 0.0f;
 
     /// <summary>
     /// 初期化
@@ -21,9 +23,13 @@ public class Opponent : MonoBehaviour
 
     public void UpdateHand()
     {
+        changeCount += Time.deltaTime;
+        if (changeCount < changeTime) return;
+
         handNum = (handNum + 1) % 3;
         Debug.Log("Opponent : handNum = " + handNum);
 
         handText.text = HandTexts[handNum];
+        changeCount = 0.0f;
     }
 }
