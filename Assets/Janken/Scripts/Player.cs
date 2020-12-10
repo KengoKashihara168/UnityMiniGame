fe11e528356a33e17a3725be8d2dbec2b1408ee2
KengoKashihara <KengoKashihara1998@gmail.com>
2020-12-10 22:36:38 +0900

    プレイヤーに手の実装
    入力された手によって数値を設定する関数を実装

--- Assets/Janken/Scripts/Player.cs
+++ Assets/Janken/Scripts/Player.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private int handNum = 0; // 手を示す数字
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
     public void Initialize()
     {
         Debug.Log("Player Initialize");
+    }
+
+    /// <summary>
+    /// プレイヤーの手を設定
+    /// </summary>
+    /// <param name="hand">入力された手</param>
+    public void SetHandType(HandType hand)
+    {
+        switch (hand)
+        {
+            case HandType.Rock:
+                handNum = 0;
+                break;
+            case HandType.Scissors:
+                handNum = 1;
+                break;
+            case HandType.Paper:
+                handNum = 2;
+                break;
+        }
+        Debug.Log(hand + " = " + handNum);
     }
 }
